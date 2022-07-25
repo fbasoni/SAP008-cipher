7195136d10bacf5ada6b1f3a314a6e8bc8314ce0
fabi basoni <basoni.fabiany@gmail.com>
2022-07-25 16:59:26 -0300

    atualização

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ function encryptLettersOnly(event){
     let toBeEncodedValue = event.target.value;
     if (toBeEncodedValue >= 32 || toBeEncodedValue <= 64 && toBeEncodedValue >= 91 || toBeEncodedValue <= 96 && toBeEncodedValue >= 123 || toBeEncodedValue <= 255){
         event.preventDefault(event);
-        window.alert("A mensagem deve conter apenas letras de A a Z, sem espaçamento, pontuação ou caracteres especiais. Por favor, digite apenas letras e tente novamente.");
+        window.alert("A palavra deve conter apenas letras de A a Z, sem espaçamento, pontuação ou caracteres especiais. Por favor, tente novamente.");
     }
     return
 })
@@ -15,19 +15,8 @@ function decryptLettersOnly(event){
     let toBeEncodedValue = event.target.value;
     if (toBeEncodedValue >= 32 || toBeEncodedValue <= 64 && toBeEncodedValue >= 91 || toBeEncodedValue <= 96 && toBeEncodedValue >= 123 || toBeEncodedValue <= 127){
         event.preventDefault(event);
-        window.alert("A mensagem deve conter apenas letras de A a Z, sem espaçamento, pontuação ou caracteres especiais. Por favor, digite apenas letras e tente novamente.");
+        window.alert("A palavra deve conter apenas letras de A a Z, sem espaçamento, pontuação ou caracteres especiais. Por favor, tente novamente.");
     }
-    return;
-})
- //não está funcionando, também bloqueia de digitar números, só parenta funcionar porque dá pra escolher os números nas setinhas, mas não dá pra digitá-lo, também aparece o alert
-document.getElementById("offsetKey").addEventListener("keypress",
-function allowNumbersOnly(event){
-    let offsetValue = event.target.value;
-    let onlyNumbers = [48, 49, 50, 51, 52, 53, 54, 55, 56, 57];
-    if (offsetValue != onlyNumbers){
-        event.preventDefault(event);
-        window.alert("O deslocamento deve ser um número de 1 a 100. Por favor, digite ou escolha um número e tente novamente.");
-    } 
     return;
 })
  
