b49a3acc3343dd8c61246b6200b51abdac4e7dbf
fabi basoni <basoni.fabiany@gmail.com>
2022-07-26 11:37:07 -0300

    updated and finished

--- src/cipher.js
+++ src/cipher.js
@@ -1,6 +1,14 @@
 const cipher = {encode, decode}
 
 function encode (offset, encryptText) {
+  if (typeof offset != "number"){
+    throw TypeError()
+  }
+
+  if (typeof encryptText != "string"){
+    throw TypeError()
+  }
+
   let encryptationResult = "";
 
   for (let i = 0; i < encryptText.length; i++){
@@ -12,6 +20,14 @@ function encode (offset, encryptText) {
 }
 
 function decode (offset, decryptText) {
+  if (typeof offset != "number"){
+    throw TypeError()
+  }
+  if (typeof decryptText != "string"){
+    throw TypeError()
+  }
+
+
   let decryptationResult = "";
 
   for (let i = 0; i < decryptText.length; i++){
