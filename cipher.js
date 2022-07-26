8ec121e665f8e2f718b716932cc876222823ea89
fabi basoni <basoni.fabiany@gmail.com>
2022-07-25 21:48:12 -0300

    Updates

--- cipher.js
+++ cipher.js
@@ -1,14 +1,8 @@
 const cipher = {encode, decode}
 
 function encode (offset, encryptText) {
-    if(typeof offset != "number") {
-      throw TypeError();
-    }
-    if (typeof encryptText != "string") {
-      throw TypeError();
-    }
-
   let encryptationResult = "";
+
   for (let i = 0; i < encryptText.length; i++){
     let encryptation = ((encryptText.charCodeAt(i) - 65 + offset) % 26) + 65;
     let encryptedText = String.fromCharCode(encryptation);
@@ -18,14 +12,8 @@ function encode (offset, encryptText) {
 }
 
 function decode (offset, decryptText) {
-    if(typeof offset != "number") {
-      throw TypeError()
-    }
-    if (typeof decryptText != "string") {
-      throw TypeError()
-    }
-
   let decryptationResult = "";
+
   for (let i = 0; i < decryptText.length; i++){
       let decryptation = ((decryptText.charCodeAt(i) - 90 - offset) % 26) + 90; 
       let decryptedText = String.fromCharCode(decryptation);
--- index.js
+++ index.js
@@ -1,21 +1,23 @@
 import cipher from './cipher.js';
 
-document.getElementById("inputEncode").addEventListener("keyup",
+const inptEncodeCheck = document.getElementById("inputEncode");
+
+inptEncodeCheck.addEventListener("keyup",
 function encryptLettersOnly(event){
     let toBeEncodedValue = event.target.value;
-    if (toBeEncodedValue >= 32 || toBeEncodedValue <= 64 && toBeEncodedValue >= 91 || toBeEncodedValue <= 96 && toBeEncodedValue >= 123 || toBeEncodedValue <= 255){
-        event.preventDefault(event);
-        window.alert("A palavra deve conter apenas letras de A a Z, sem espaçamento, pontuação ou caracteres especiais. Por favor, tente novamente.");
+    if ((toBeEncodedValue >= 33 || toBeEncodedValue <= 64) && (toBeEncodedValue >= 91 || toBeEncodedValue <= 127)){
+        window.alert("Oops! Eu só cifro palavras. Por favor, digite apenas letras de A a Z (sem pontuação, acentos ou caracteres especiais) e tente novamente.");
     }
     return
 })
 
-document.getElementById("inputDecode").addEventListener("keyup",
+const inptDecodeCheck = document.getElementById("inputDecode");
+
+inptDecodeCheck.addEventListener("keyup",
 function decryptLettersOnly(event){
-    let toBeEncodedValue = event.target.value;
-    if (toBeEncodedValue >= 32 || toBeEncodedValue <= 64 && toBeEncodedValue >= 91 || toBeEncodedValue <= 96 && toBeEncodedValue >= 123 || toBeEncodedValue <= 127){
-        event.preventDefault(event);
-        window.alert("A palavra deve conter apenas letras de A a Z, sem espaçamento, pontuação ou caracteres especiais. Por favor, tente novamente.");
+    let toBeDecodedValue = event.target.value;
+    if ((toBeDecodedValue >= 33 || toBeDecodedValue <= 64) && (toBeDecodedValue >= 91 || toBeDecodedValue <= 127)){
+        window.alert("Oops! Eu só decifro palavras. Por favor, digite apenas letras de A a Z (sem pontuação, acentos ou caracteres especiais) e tente novamente.");
     }
     return;
 })
@@ -23,22 +25,22 @@ function decryptLettersOnly(event){
 const btnEncode = document.getElementById("encodeButton");
 
 btnEncode.addEventListener("click", 
-function toEncrypt(){
+function encrypt(){
     let toBeEncoded = document.getElementById("inputEncode").value;
     toBeEncoded = toBeEncoded.toUpperCase();
     let shiftingAmount = parseInt(document.getElementById("offsetKey").value);
     let encodeResult = cipher.encode(shiftingAmount, toBeEncoded);
-    document.getElementById("resultado").innerHTML = "A mensagem criptografada é: " + '"' + encodeResult + '"';
+    document.getElementById("displayedResult").innerHTML = "A mensagem criptografada é: " + '"' + encodeResult + '"';
 })
 
 const btnDecode = document.getElementById("decodeButton");
 
 btnDecode.addEventListener("click", 
-function toDecrypt(){
+function decrypt(){
     let toBeDecoded = document.getElementById("inputDecode").value;
     toBeDecoded = toBeDecoded.toUpperCase();
     let shiftingAmount = parseInt(document.getElementById("offsetKey").value);
     let decodeResult = cipher.decode(shiftingAmount, toBeDecoded);
-    document.getElementById("resultado").innerHTML = "A mensagem descriptografada é: " + '"' + decodeResult + '"'
+    document.getElementById("displayedResult").innerHTML = "A mensagem descriptografada é: " + '"' + decodeResult + '"';
 })
 
