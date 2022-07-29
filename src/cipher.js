4757b37bb78678a856de67ba87b34f0d3f4c7983
fabi basoni <basoni.fabiany@gmail.com>
2022-07-29 07:50:27 -0300

    updated (renamed some variables for a cleaner code, deleted comments)

--- src/cipher.js
+++ src/cipher.js
@@ -5,11 +5,7 @@ function encode (offset, encodeText) {
     throw new TypeError("Invalid data type.") 
   }
 
-  let encodeResult = "";                                                     //letra que quero decifrar.charCodeAt
-//quando eu subtraio o código da letra que quero decifrar pelo código da primeira (A=65), ( String.charCodeAt(0) - 65 ) eu faço um deslocamento do alfabeto, me dando a posição das letras no alfabeto
-//de acordo com a cifra. por ex: "B".charCodeAt(0) - 65 = 66 - 65 => 1 (logo, posição do B é 1)
-//string => "bola" => "dqnc"
-//offset = 2;
+  let encodeResult = "";                                                    
 
   for (let i = 0; i < encodeText.length; i++){
     let asciiEncode = ((encodeText.charCodeAt(i) - 65 + offset) % 26) + 65;
@@ -25,8 +21,7 @@ function decode (offset, decodeText) {
   }
 
   let decodeResult = "";
-//bola
-//offset = 2;
+
   for (let i = 0; i < decodeText.length; i++){
       let asciiDecode = ((decodeText.charCodeAt(i) - 90 - offset) % 26) + 90; 
       let decodedText = String.fromCharCode(asciiDecode);
